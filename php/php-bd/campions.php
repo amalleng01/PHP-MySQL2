7dc911e00cc757b83317168e0e449eafee03e129
Alejandro <amalleng01@elpuig.xeill.net>
2021-03-12 12:50:11 +0100

    Add files via upload

---
+++ php/php-bd/campions.php
@@ -0,0 +1,154 @@
+<?php
+    include 'controlador.php';
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Campions</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="styleGestor.css" rel="stylesheet"/>
+        <script src=""></script>
+    </head>
+    <body>
+	<header>
+            <h1>Gestor Campions</h1>
+	</header>
+	
+            <div class="crearModificarJugador">
+                <h3>Crear/Modificar Jugador</h3>
+                <form action="campions.php" method="post">
+                    <input type="hidden" name="form" value="jugador">
+                    <input type="text" name="id" placeholder="Id" /><br /><br />
+                    <input type="text" name="nombre" placeholder="Nom"/><br /><br />
+                    <input type="number" name="nivel" placeholder="Nivell"/><br /><br />
+                    <input type="date" name="fecha" placeholder="Data alta" /><br /><br />
+                    <label><input type="radio" name="accion" value="crear" checked> Crear</label><br />
+                    <label><input type="radio" name="accion" value="modificar"> Modificar<br /></label><br />
+                    <input type="submit" value="Aceptar" name="btnJugador">
+                </form>
+                
+            </div>
+            <div class="crearModificarCampio">
+                <h3>Crear/Modificar Campió</h3>
+                <form action="campions.php" method="post">
+                    <input type="hidden" name="form" value="campio">
+                    <input type="text" name="id" placeholder="Id"/><br /><br />
+                    <input type="text" name="nombre" placeholder="Nom"/><br /><br />
+                    <input type="text" name="tipo" placeholder="Tipus"/><br /><br />
+                    <input type="number" name="precio" placeholder="Preu" /><br /><br />
+                    <input type="date" name="fecha" placeholder="Data alta" /><br /><br />
+                    <label><input type="radio" name="accion" value="crear" checked> Crear</label><br />
+                    <label><input type="radio" name="accion" value="modificar"> Modificar<br /></label><br />
+                    <input type="submit" value="Aceptar">
+                </form>
+            </div>
+            <div class="crearModificarBatalla">
+                <h3>Crear / Modificar Batalla</h3>
+                <form action="campions.php" method="post">
+                    <input type="hidden" name="form" value="batalla">
+                    <input type="text" name="idJ" placeholder="Id jugador"/><br /><br />
+                    <input type="text" name="idC" placeholder="Id Campió"/><br /><br />
+                    <input type="number" name="cantidad" placeholder="Cantitat" /><br /><br />
+                    <label><input type="radio" name="accion" value="crear" checked> Crear</label><br />
+                    <label><input type="radio" name="accion" value="modificar"> Modificar<br /></label><br />
+                    <input type="submit" value="Aceptar">
+                </form>
+            </div>
+            <div class="eliminar">
+                <h3>Eliminar</h3>
+                <form action="campions.php" method="post">
+                    <input type="hidden" name="form" value="jugador">
+                    <input type="hidden" name="accion" value="eliminar">
+                    <input type="text" name="id" placeholder="Id jugador"/><br />
+                    <input type="submit" value="Eliminar"><br>
+                </form>
+                <form action="campions.php" method="post">
+                    <input type="hidden" name="form" value="campio">
+                    <input type="hidden" name="accion" value="eliminar">
+                    <input type="text" name="id" placeholder="Id campió"/><br />
+                    <input type="submit" value="Eliminar"><br>
+                </form>
+                <form action="campions.php" method="post">
+                    <input type="hidden" name="form" value="batalla">
+                    <input type="hidden" name="accion" value="eliminar">
+                    <input type="text" name="idJ" placeholder="Id jugador"/><br /><br />
+                    <input type="text" name="idC" placeholder="Id campió"/><br /><br />
+                    <input type="submit" value="Eliminar">
+                </form>
+                
+            </div>
+	
+        
+            <div class="jugadors">
+                <h3>Jugadors</h3>
+                <?php
+                 $jugadores = getJugadores($bd);
+                     if( !is_null($jugadores) ){
+                       echo "<table class='tableJugador'>";
+                       echo '<tr><td>Id</td><td>Nom</td><td>Nivell</td><td>Data</td></tr>';
+                           for ($i = 0; $i < $jugadores->numFilasObtenidas; $i++){
+                               echo "<tr><td>".$jugadores->tablaResultados[$i]->id.
+                                   "</td><td>".$jugadores->tablaResultados[$i]->nombre.
+                                   "</td><td>".$jugadores->tablaResultados[$i]->nivel.
+                                   "</td><td>".$jugadores->tablaResultados[$i]->fecha."</td></tr>";
+                           
+                           
+                           echo "</table>";
+                           }
+                     }
+                ?>
+
+                
+            </div>
+            <div class="relacions">
+                <h3>Batalles</h3>
+                <?php
+                    $batallas = getBatallas($bd);
+                    if( !is_null($batallas) ){
+                        echo '<table class="tablaBatalla">';
+                        echo '<tr><th>Id Jugador</th><th>Id Campeón</th><th>Cantidad</th>';
+                        
+                        // Bucle foreach que recorre cada posicion del array $batallas
+                        // y escribe una fila con tantas columnas como atributos tiene un una batalla
+                        for ($i = 0; $i < $batallas->numFilasObtenidas; $i++){
+                            echo "<tr><td>".$batallas->tablaResultados[$i]->idJugador.
+                                "</td><td>".$batallas->tablaResultados[$i]->idCampeon.
+                                "</td><td>".$batallas->tablaResultados[$i]->cantidad."</td></tr>";
+                              
+                        echo '</table>';
+                    }
+                }
+
+                    else {echo "Tabla Batallas vacía";}
+                ?>
+            </div>
+            <div class="campions">
+                <h3>Campions</h3>
+                <?php
+                    $campeones = getCampeones($bd);
+                    if(!is_null($campeones) ){
+                        echo '<table class="tablaCampeon">';
+                        echo '<tr><th class="campeon">Id</th><th>Nom</th><th>Tipus</th><th>Preu</th><th>Data</th></tr>';
+                        
+                        // Bucle foreach que recorre cada posicion del array $campeones
+                        // y escribe una fila con tantas columnas como atributos tiene un campeon
+                        for ($i = 0; $i < $campeones->numFilasObtenidas; $i++){
+                            echo "<tr><td>".$campeones->tablaResultados[$i]->id.
+                                "</td><td>".$campeones->tablaResultados[$i]->nombre.
+                                "</td><td>".$campeones->tablaResultados[$i]->tipo.
+                                "</td><td>".$campeones->tablaResultados[$i]->precio.
+                                "</td><td>".$campeones->tablaResultados[$i]->fecha."</td></tr>";
+                        
+                        echo '</table>';
+                    } 
+                }
+                    else {echo "Tabla Campeones vacía";}
+                ?>
+            </div>
+	
+    </body>
+</html>
+
---
+++ php/php-bd/controlador.php
@@ -0,0 +1,237 @@
+<?php
+
+// Incluir las clases MySQL y Consulta
+include 'MySQL.php';
+include 'consulta.php';
+
+// Conectamos con la base de datos
+$bd = new MySQL("admin", "admin", "192.168.0.29", "campions");
+
+
+
+$post;
+
+
+$jugadorArgs = array(
+    'id'        => FILTER_SANITIZE_ENCODED,
+    'nombre'    => FILTER_SANITIZE_ENCODED,
+    'nivel'     => FILTER_SANITIZE_ENCODED,
+    'fecha'     => FILTER_SANITIZE_ENCODED,
+);
+
+$campioArgs = array(
+    'id'        => FILTER_SANITIZE_ENCODED,
+    'nombre'    => FILTER_SANITIZE_ENCODED,
+    'tipo'      => FILTER_SANITIZE_ENCODED,
+    'precio'    => FILTER_SANITIZE_ENCODED,
+    'fecha'     => FILTER_SANITIZE_ENCODED,
+);
+
+$batallaArgs = array(
+    'idJ'        => FILTER_SANITIZE_ENCODED,
+    'idC'        => FILTER_SANITIZE_ENCODED,
+    'cantidad'   => FILTER_SANITIZE_ENCODED,
+);
+
+//declaración variables globales-----------
+
+
+
+
+//switch controler form------------------------------------
+
+
+if(!empty($_POST)){
+    action_campions($post, $bd);
+}
+
+function action_campions($post, $bd){
+    switch ($_POST) {
+        case $_POST["form"] == "jugador":
+            global $jugadorArgs;
+           //permite introducir los datos del formulario a la query
+            $post=filter_input_array(INPUT_POST, $jugadorArgs);
+              
+            if($_POST["accion"] == 'crear'){insertJugador($post, $bd);}
+            else if($_POST["accion"] == 'eliminar'){deleteJugador($post, $bd);}
+            else if($_POST["accion"] == 'modificar'){updateJugador($post, $bd);}
+
+            break;
+        case $_POST["form"] == 'campio':
+            global $campioArgs;
+            $post=filter_input_array(INPUT_POST, $campioArgs);
+
+            if($_POST["accion"] == 'crear'){insertCampeon($post, $bd);}
+            else if($_POST["accion"] == 'eliminar'){deleteCampeon($post, $bd);}
+            else if($_POST["accion"] == 'modificar'){updateCampeon($post, $bd);}
+      
+            break;
+        case $_POST["form"] == 'batalla':
+            global $batallaArgs;
+            $post=filter_input_array(INPUT_POST, $batallaArgs);
+
+            if($_POST["accion"] == 'crear'){insertBatalla($post, $bd);}
+            else if($_POST["accion"] == 'eliminar'){deleteBatalla($post, $bd);}
+            else if($_POST["accion"] == 'modificar'){updateBatalla($post, $bd);}
+           
+            break;
+        default:
+           echo"error switch";
+    }
+
+}
+
+
+//jugador------------------------------------------------------------------------------------------------------------------------------
+//INSERT JUGADOR
+
+
+function insertJugador($jugadorinsert, $bd){
+    $id=$jugadorinsert['id'];
+    $nombre=$jugadorinsert['nombre'];
+    $nivel=(int)$jugadorinsert['nivel'];
+    $fecha=$jugadorinsert['fecha'];
+
+    $query = "INSERT INTO jugador (id, nombre, nivel, fecha) VALUES ('$id', '$nombre', '$nivel', '$fecha')";
+    new Consulta($query, $bd);
+}
+
+//DELETE JUGADOR
+
+function deleteJugador($jugadordelete, $bd){
+    $id=$jugadordelete['id'];
+
+    $query = "DELETE FROM jugador WHERE id=$id";
+    new Consulta($query, $bd);
+}
+
+
+//UPDATE JUGADOR
+
+function updateJugador($jugadorupdate, $bd){
+    $id=$jugadorupdate['id'];
+    $nombre=$jugadorupdate['nombre'];
+    $nivel=(int)$jugadorupdate['nivel'];
+    $fecha=$jugadorupdate['fecha'];
+
+    $query = "UPDATE jugador SET nombre='$nombre', nivel='$nivel', fecha='$fecha' WHERE id=$id";
+    new Consulta($query, $bd);
+}
+
+
+
+//campeon--------------------------------------------------------------------------------------------------------------------
+
+//INSERT CAMPEON
+
+    function insertCampeon($campioinsert, $bd){
+        $id=$campioinsert['id'];
+        $nombre=$campioinsert['nombre'];
+        $tipo=$campioinsert['tipo'];
+        $precio=(int)$campioinsert['precio'];
+        $fecha=$campioinsert['fecha'];
+    
+        $query = "INSERT INTO campeon (id, nombre, tipo, precio, fecha) VALUES ('$id', '$nombre', '$tipo', '$precio', '$fecha')";
+        new Consulta($query, $bd);
+    }
+
+//DELETE CAMPEON
+function deleteCampeon($campiodelete, $bd){
+    $id=$campiodelete['id'];
+
+    $query = "DELETE FROM campeon WHERE id=$id";
+    new Consulta($query, $bd);
+}
+
+
+//UPDATE CAMPEON
+
+function updateCampeon($campioupdate, $bd){
+    $id=$campioupdate['id'];
+    $nombre=$campioupdate['nombre'];
+    $tipo=$campioupdate['tipo'];
+    $precio=(int)$campioupdate['precio'];
+    $fecha=$campioupdate['fecha'];
+
+    $query = "UPDATE campeon SET nombre='$nombre', tipo='$tipo', precio='$precio', fecha='$fecha' WHERE id=$id";
+    new Consulta($query, $bd);
+}
+
+
+
+
+
+
+
+//btalla-------------------------------------------------------------------------------------------------------------------
+
+
+
+//INSERT BATALLA
+
+
+function insertBatalla($batallainsert, $bd){
+    $idJugador=$batallainsert['idJ'];
+    $idCampeon=$batallainsert['idC'];
+    $cantidad=(int)$batallainsert['cantidad'];
+
+    $query = "INSERT INTO batalla (idJugador, idCampeon, cantidad) VALUES ('$idJugador', '$idCampeon', '$cantidad')";
+    new Consulta($query, $bd);
+}
+
+//DELETE BATALLA
+
+
+function deleteBatalla($batalladelete, $bd){
+    $idJugador=$batalladelete['idJ'];
+    $idCampeon=$batalladelete['idC'];
+
+    $query = "DELETE FROM batalla WHERE idJugador='$idJugador' AND idCampeon='$idCampeon'";
+    new Consulta($query, $bd);
+}
+
+//UPDATE BATALLA
+
+
+function updateBatalla($batallaupdate, $bd){
+    $idJugador=$batallaupdate['idJ'];
+    $idCampeon=$batallaupdate['idC'];
+    $cantidad=(int)$batallaupdate['cantidad'];
+    
+    $query = "UPDATE batalla SET cantidad='$cantidad' WHERE idJugador='$idJugador' AND idCampeon='$idCampeon'";
+    new Consulta($query, $bd);
+}
+
+
+
+
+//GET DE LAS TABLAS---------------------------
+//GET JUGADORES
+function getJugadores($bd){
+    $query = ("SELECT * FROM jugador");
+    $query = new Consulta($query, $bd);
+    return $query;
+    
+}
+
+//GET CAMPEONES
+function getCampeones($bd){
+    $query = ("SELECT * FROM campeon");
+    $query = new Consulta($query, $bd);
+    return $query;
+}
+
+//GET BATALLAS
+function getBatallas($bd){
+    $query = ("SELECT * FROM batalla");
+    $query = new Consulta($query, $bd);
+    return $query;
+}
+//-------------------------------------------
+
+
+
+
+
+
+?>
